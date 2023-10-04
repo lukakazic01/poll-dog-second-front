4834497d3511c3e72c744e41fbdb44548057cf25
SoftLab Applicant <kazicluka4@gmail.com>
2023-10-04 16:25:50 +0200

    added error and loading states

---
+++ index.js
@@ -0,0 +1,39 @@
+const loader = document.querySelector(".loader-container");
+const errorContainer = document.querySelector(".error-container");
+const barChart = document.getElementById("chart");
+    (async () => {
+        try {
+        loader.style.display = 'flex';
+        const data = await fetch("https://localhost:7070/PollDogAPI")
+        const formatedData = await data.json()
+        const allProducts = formatedData.map((item) => item.product)
+        const brandsRating = formatedData.map((item) => item.average_rating)
+        const barColors= formatedData.map((item) => `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`)
+        new Chart("chart", {
+            type: "bar",
+            data: {
+                labels: allProducts,
+                datasets: [{
+                    label: 'PollDog Insights',
+                    backgroundColor: barColors,
+                    data: brandsRating
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+        } catch (err) {
+            barChart.style.display = 'none';
+            errorContainer.style.visibility = 'visible';
+            console.log(err)
+        } finally {
+            loader.style.display = 'none';
+        }
+    })()
